ce1cdab9570ec68faeb294ac08b300919a970d37
TanKhuu <jackiekhuu.work@gmail.com>
2017-02-22 22:31:55 +0700

    use babel to build with es2015

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-var _  = require('lodash');
+'use strict';
+
+var _ = require('lodash');
 var moment = require('moment');
 var S = require('string');
 
@@ -9,212 +11,214 @@ var S = require('string');
  * @param {String} operator
  * @returns {function()}
  */
-const create = (type, operator) => {
-  const
-    supportedTypes = ['number', 'date', 'boolean', 'string'],
-    supportedOperators = {
-      number: ['equal', 'notEqual', 'lessThan', 'lessThanOrEqual', 'greaterThan', 'greaterThanOrEqual', 'between'],
-      date: ['on', 'before', 'after', 'onOrBefore', 'onOrAfter', 'within'],
-      boolean: ['equal', 'notEqual'],
-      string: ['is', 'startWith', 'contains'],
-    },
-    isSupportedType = _.indexOf(supportedTypes, type),
-    isSupportedOperator = _.indexOf(supportedOperators[type], operator)
-    ;
+var create = function create(type, operator) {
+  var supportedTypes = ['number', 'date', 'boolean', 'string'],
+      supportedOperators = {
+    number: ['equal', 'notEqual', 'lessThan', 'lessThanOrEqual', 'greaterThan', 'greaterThanOrEqual', 'between'],
+    date: ['on', 'before', 'after', 'onOrBefore', 'onOrAfter', 'within'],
+    boolean: ['equal', 'notEqual'],
+    string: ['is', 'startWith', 'contains']
+  },
+      isSupportedType = _.indexOf(supportedTypes, type),
+      isSupportedOperator = _.indexOf(supportedOperators[type], operator);
 
   // verify if the type is supported or not
   if (isSupportedType < 0) {
-    throw new Error('UNSUPPORTED_TYPE', `type: ${type} is unsupported to create condition`);
+    throw new Error('UNSUPPORTED_TYPE', 'type: ' + type + ' is unsupported to create condition');
   }
 
   // verify if the operator is supported or not
   if (isSupportedOperator < 0) {
-    throw new Error('UNSUPPORTED_OPERATOR', `type: ${operator} is unsupported to create condition`);
+    throw new Error('UNSUPPORTED_OPERATOR', 'type: ' + operator + ' is unsupported to create condition');
   }
 
   switch (type) {
     case 'number':
-    {
-      switch (operator) {
-        case 'equal':
-        {
-          return (...args) => {
-            console.log(args);
-            return args[0] === args[1];
-          };
-        }
-        case 'notEqual':
-        {
-          return (...args) => {
-            return args[0] !== args[1];
-          };
-        }
-        case 'lessThan':
-        {
-          return (...args) => {
-            return args[0] < args[1];
-          };
-        }
-        case 'lessThanOrEqual':
-        {
-          return (...args) => {
-            return args[0] <= args[1];
-          };
-        }
-        case 'greaterThan':
-        {
-          return (...args) => {
-            return args[0] > args[1];
-          };
-        }
-        case 'greaterThanOrEqual':
-        {
-          return (...args) => {
-            return args[0] >= args[1];
-          };
-        }
-        case 'between':
-        {
-          return (...args) => {
-            return (args[0] >= args[1] && args[0] <= args[2]);
-          };
-        }
-        default:
-        {
-          throw new Error('UNKNOWN_OPERATOR', `Unknown operator: ${operator} of type: ${type}`);
+      {
+        switch (operator) {
+          case 'equal':
+            {
+              return function (args) {
+                console.log(args);
+                return args[0] === args[1];
+              };
+            }
+          case 'notEqual':
+            {
+              return function (args) {
+                return args[0] !== args[1];
+              };
+            }
+          case 'lessThan':
+            {
+              return function (args) {
+                return args[0] < args[1];
+              };
+            }
+          case 'lessThanOrEqual':
+            {
+              return function (args) {
+                return args[0] <= args[1];
+              };
+            }
+          case 'greaterThan':
+            {
+              return function (args) {
+                return args[0] > args[1];
+              };
+            }
+          case 'greaterThanOrEqual':
+            {
+              return function (args) {
+                return args[0] >= args[1];
+              };
+            }
+          case 'between':
+            {
+              return function (args) {
+                return args[0] >= args[1] && args[0] <= args[2];
+              };
+            }
+          default:
+            {
+              throw new Error('UNKNOWN_OPERATOR', 'Unknown operator: ' + operator + ' of type: ' + type);
+            }
         }
       }
-    }
     case 'date':
-    {
-      switch (operator) {
-        case 'on':
-        {
-          return (...args) => {
-            // unit is in year | month | week | day | hour | minute | second
-            // verify date type
-            /* ... to do verify
-             *  */
+      {
+        switch (operator) {
+          case 'on':
+            {
+              return function (args) {
+                // unit is in year | month | week | day | hour | minute | second
+                // verify date type
+                /* ... to do verify
+                 *  */
 
-            return moment(args[0]).isSame(args[1], args[2]);
-          };
-        }
-        case 'before':
-        {
-          return (...args) => {
-            // verify date type
-            /* ... to do verify
-             *  */
+                return moment(args[0]).isSame(args[1], args[2]);
+              };
+            }
+          case 'before':
+            {
+              return function (args) {
+                // verify date type
+                /* ... to do verify
+                 *  */
 
-            return moment(args[0]).isBefore(args[1], args[2]);
-          };
-        }
-        case 'onOrBefore':
-        {
-          return (...args) => {
-            // verify date type
-            /* ... to do verify
-             *  */
+                return moment(args[0]).isBefore(args[1], args[2]);
+              };
+            }
+          case 'onOrBefore':
+            {
+              return function (args) {
+                // verify date type
+                /* ... to do verify
+                 *  */
 
-            return moment(args[0]).isSameOrBefore(args[1], args[2]);
-          };
-        }
-        case 'after':
-        {
-          return (...args) => {
-            // verify date type
-            /* ... to do verify
-             *  */
+                return moment(args[0]).isSameOrBefore(args[1], args[2]);
+              };
+            }
+          case 'after':
+            {
+              return function (args) {
+                // verify date type
+                /* ... to do verify
+                 *  */
 
-            return moment(args[0]).isAfter(args[1], args[2]);
-          };
-        }
-        case 'onOrAfter':
-        {
-          return (...args) => {
-            // verify date type
-            /* ... to do verify
-             *  */
+                return moment(args[0]).isAfter(args[1], args[2]);
+              };
+            }
+          case 'onOrAfter':
+            {
+              return function (args) {
+                // verify date type
+                /* ... to do verify
+                 *  */
 
-            return moment(args[0]).isSameOrAfter(args[1], args[2]);
-          };
-        }
-        case 'within':
-        {
-          return (...args) => {
-            // verify date type
-            /* ... to do verify
-             *  */
+                return moment(args[0]).isSameOrAfter(args[1], args[2]);
+              };
+            }
+          case 'within':
+            {
+              return function (args) {
+                // verify date type
+                /* ... to do verify
+                 *  */
 
-            return moment(args[0]).isBetween(args[1], args[2]);
-          };
-        }
-        default:
-        {
-          throw new Error('UNKNOWN_OPERATOR', `Unknown operator: ${operator} of type: ${type}`);
+                return moment(args[0]).isBetween(args[1], args[2]);
+              };
+            }
+          default:
+            {
+              throw new Error('UNKNOWN_OPERATOR', 'Unknown operator: ' + operator + ' of type: ' + type);
+            }
         }
       }
-    }
     case 'boolean':
-    {
-      switch (operator) {
-        case 'equal':
-        {
-          return (arg1, arg2) => {
-            return arg1 === arg2;
-          };
-        }
-        case 'notEqual':
-        {
-          return (arg1, arg2) => {
-            return arg1 !== arg2;
-          };
-        }
-        default:
-        {
-          throw new Meteor.Error('UNKNOWN_OPERATOR', `Unknown operator: ${operator} of type: ${type}`);
+      {
+        switch (operator) {
+          case 'equal':
+            {
+              return function (args) {
+                return args[0] === args[1];
+              };
+            }
+          case 'notEqual':
+            {
+              return function (args) {
+                return args[0] !== args[0];
+              };
+            }
+          default:
+            {
+              throw new Meteor.Error('UNKNOWN_OPERATOR', 'Unknown operator: ' + operator + ' of type: ' + type);
+            }
         }
       }
-    }
     case 'string':
-    {
-      switch (operator) { // don't care about the capital
-        case 'is':
-        {
-          return (...args) => {
-            return args[0].toLowerCase() === args[1].toLowerCase();
-          };
-        }
-        case 'startWith':
-        {
-          return (...args) => {
-            S(args[0]).startsWith(args[1]);
-          };
-        }
-        case 'contains':
-        {
-          return (...args) => {
-            return S(args[0]).contains(args[1]);
-          };
-        }
-        default:
-        {
-          throw new Error('UNKNOWN_OPERATOR', `Unknown operator: ${operator} of type: ${type}`);
+      {
+        switch (operator) {// don't care about the capital
+          case 'is':
+            {
+              return function (args) {
+                return args[0].toLowerCase() === args[1].toLowerCase();
+              };
+            }
+          case 'startWith':
+            {
+              return function (args) {
+                S(args[0]).startsWith(args[1]);
+              };
+            }
+          case 'contains':
+            {
+              return function (args) {
+                return S(args[0]).contains(args[1]);
+              };
+            }
+          default:
+            {
+              throw new Error('UNKNOWN_OPERATOR', 'Unknown operator: ' + operator + ' of type: ' + type);
+            }
         }
       }
-    }
     default:
-    {
-      throw new Error('UNKNOWN_TYPE', `Unknown type: ${type}`);
-    }
+      {
+        throw new Error('UNKNOWN_TYPE', 'Unknown type: ' + type);
+      }
   }
 };
 
-const compare = (type, operator, ...args) => {
+var compare = function compare(type, operator) {
+  for (var _len = arguments.length, args = Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {
+    args[_key - 2] = arguments[_key];
+  }
+
   return create(type, operator)(args);
 };
 
 module.exports = {
-  create,
-  compare
+  create: create,
+  compare: compare
 };
